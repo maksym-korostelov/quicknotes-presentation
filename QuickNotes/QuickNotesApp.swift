28496ef80f460a658e8e733a2ae86be5528ab32d
Maksym Korostelov <Maksym_Korostelov@epam.com>
2026-01-29 05:58:24 +0100

    Initial Commit

---
+++ QuickNotes/QuickNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickNotesApp.swift
+//  QuickNotes
+//
+//  Created by Maksym Korostelov on 29/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct QuickNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
